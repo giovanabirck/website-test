9a92254e93a34fb5a08bb23cd4b4cab7b8e972e5
Giovana Birck <gmattosbirck@my.bcit.ca>
2025-04-01 18:35:45 -0700

    options

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ function App() {
         <h1>Giovana Birck</h1>
       </div>
 
+      <div>
+        <h2>Product Design</h2>
+        <h2>Graphic Design</h2>
+        <h2>Spatial Design</h2>
+        <h2>Visual Art</h2>
+      </div>
+
       <div className='footer'>
         <p>contact@giovanabirck.com</p>
       </div>
