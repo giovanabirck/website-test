e201f069e1018bdd95cdc6c1c051e999c294ac0d
Giovana Birck <gmattosbirck@my.bcit.ca>
2025-04-08 13:05:49 -0700

    image hover

--- src/pages/product-design/ProductDesign.js
+++ src/pages/product-design/ProductDesign.js
@@ -105,16 +105,16 @@ function ProductDesign() {
 
             <div className='project'>
               <img src='/product_design/adp_hr_actions.jpg' alt='HR Actions'/>
-              <h4>Transforming ADP's HR Actions with a Scalable Design System</h4>
+              <h4>Enhancing the Nomadic Experience with Network Recommendations</h4>
               <ul className='project_details'>
-                <li>shipped</li>
-                <li>desktop</li>
+                <li>in development</li>
+                <li>mobile</li>
               </ul>
             </div>
 
             <div className='project'>
               <img src='/product_design/adp_hr_actions.jpg' alt='HR Actions'/>
-              <h4>Transforming ADP's HR Actions with a Scalable Design System</h4>
+              <h4>Improving Usability in MyADP's Task Management Tool, Things to Do</h4>
               <ul className='project_details'>
                 <li>shipped</li>
                 <li>desktop</li>
