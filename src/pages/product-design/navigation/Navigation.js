4216de4a4ad1bee0bf87417c5f19958ded3b163a
Giovana Birck <gmattosbirck@my.bcit.ca>
2025-04-09 11:21:22 -0700

    fixing useState

--- src/pages/product-design/navigation/Navigation.js
+++ src/pages/product-design/navigation/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useState } from 'react-router-dom';
 import './Navigation.modules.css';
 
 function Navigation() {
