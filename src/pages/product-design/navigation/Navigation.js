e1ed7db51e14cd36e6a0b54fb94c15ac674aeabc
Giovana Birck <gmattosbirck@my.bcit.ca>
2025-04-09 12:02:48 -0700

    fixing bugs

--- src/pages/product-design/navigation/Navigation.js
+++ src/pages/product-design/navigation/Navigation.js
@@ -1,9 +1,10 @@
 import React from 'react';
-// import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Navigation.modules.css';
 
-function Navigation({ activeTab, activeStyles }) {
+function Navigation() {
+    const location = useLocation();
+    const isActive = (path) => location.pathname === path ? 'active' : '';
 
     return (
         <div className="navigation">
